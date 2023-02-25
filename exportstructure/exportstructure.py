a01e47e95c2d164ffe50c2a9df84fb05c15a30c2
Yuji Ishikawa <yuji2iskwutrt@gmail.com>
2023-02-25 19:14:30 +0900

    support array of array

--- exportstructure/exportstructure.py
+++ exportstructure/exportstructure.py
@@ -1,41 +1,49 @@
 def print_item(cstrt, prefix):
+    def print_ary(cary, prefix):
+        # Array of X
+        if hasattr(cary[0], "_fields_"):
+            # Array of Struct
+            for atr in cary:
+                print_item(atr, prefix + "  ")
+        elif hasattr(cary[0], "_length_"):
+            # Array of Array
+            for atr in cary:
+                print_ary(atr, prefix + "  ")
+        else:
+            # member is Array of Scalar
+            print(prefix + "  " + ', '.join(map(str, cary)))
+
     for (name, classid) in cstrt._fields_:
         attr = getattr(cstrt, name)
         print(prefix + "{}, {},{}".format(name, classid, attr))
         if hasattr(attr, "_length_"):
             # member is Array of X
-            if hasattr(attr[0], "_fields_"):
-                # member is Array of Struct
-                for atr in attr:
-                    print_item(atr, prefix + "  ")
-            elif hasattr(attr[0], "_length_"):
-                # member is Array of Array
-                print("no support for Array of Array")
-            else:
-                # member is Array of Scalar
-                print(prefix + "  " + ', '.join(map(str, attr)))
+            print_ary(attr, prefix + "  ")
         elif hasattr(attr, "_fields_"):
             # member is Struct
             print_item(attr, prefix + "  ")
 
 def export_item(cstrt):
+    def export_ary(cary):
+        # Array of X
+        ret = []
+        if hasattr(cary[0], "_fields_"):
+            for atr in cary:
+                ret.append(export_item(atr))
+        elif hasattr(cary[0], "_length_"):
+            for atr in cary:
+                ret.append(export_ary(atr))
+        else:
+            for atr in cary:
+                ret.append(int(atr))
+        return ret
+
     ret = {}
     for (name, classid) in cstrt._fields_:
         attr = getattr(cstrt, name)
         if hasattr(attr, "_length_"):
             # member is Array of X
-            ret[name] = []
-            if hasattr(attr[0], "_fields_"):
-                # member is Array of Structure
-                for atr in attr:
-                    ret[name].append(export_item(atr))
-            elif hasattr(attr[0], "_length_"):
-                # member is Array of Array
-                print("no support for Array of Array")
-            else:
-                # member is Array of Scalar
-                for atr in attr:
-                    ret[name].append(int(atr))
+            ret[name] = export_ary(attr)
         elif hasattr(attr, "_fields_"):
             # member is Structure
             ret[name] = export_item(attr)
@@ -45,21 +53,26 @@ def export_item(cstrt):
     return ret
 
 def import_item(cstrt, obj):
+    def import_ary(cary, obj):
+        # Array of X
+        if hasattr(cary[0], "_fields_"):
+            # Array of Structure
+            for idx, atr in enumerate(cary):
+                import_item(atr, obj[idx])
+        elif hasattr(cary[0], "_length_"):
+            # Array of Array
+            for idx, atr in enumerate(cary):
+                import_ary(atr, obj[idx])
+        else:
+            # Array of Scalar
+            for idx, atr in enumerate(cary):
+                cary[idx] = obj[idx]
+
     for(name, classid) in cstrt._fields_:
         attr = getattr(cstrt, name)
         if hasattr(attr, "_length_"):
             # member is Array of X
-            if hasattr(attr[0], "_fields_"):
-                # member is Array of Structure
-                for idx, atr in enumerate(attr):
-                    import_item(atr, obj[name][idx])
-            elif hasattr(attr[0], "_length_"):
-                # member is Array of Array
-                print("no support for Array of Array")
-            else:
-                # member is Array of Scalar
-                for idx, atr in enumerate(attr):
-                    getattr(cstrt, name)[idx] = obj[name][idx]
+            import_ary(attr, obj[name])
         elif hasattr(attr, "_fields_"):
             # member is Struct
             import_item(attr, obj[name])
